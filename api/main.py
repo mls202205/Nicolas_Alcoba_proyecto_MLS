1df8643870f0d46a9d8519256dcbe1f2af8ab2c5
NicolasAMrvk <nicolas@marvik.ai>
2022-06-17 12:16:22 -0300

    Get and dynamic filters

--- api/main.py
+++ api/main.py
@@ -1,6 +1,9 @@
-from . import database, crud
+import array
+from sqlalchemy.orm import Session
+from . import crud, schema
 
-from fastapi import Depends, FastAPI, Request, Response
+from typing import List
+from fastapi import Depends, FastAPI, File, Form, HTTPException, Query, Request, Response
 import uvicorn
 
 
@@ -23,7 +26,7 @@ You will be able to:
 )
 
 
-@app.middleware("http")
+"""@app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
     response = Response("Internal server error", status_code=500)
     try:
@@ -31,13 +34,30 @@ async def db_session_middleware(request: Request, call_next):
         response = await call_next(request)
     finally:
         request.state.db.close()
-    return response
-
+    return response"""
 @app.get("/debug", tags=["debug"])
-def debug(db: database.Session = Depends(crud.get_db)):
+def debug():
     import pdb; pdb.set_trace()
 
 
+@app.get("/data/pages/{page}", tags=["get"])
+def get_by_page(page: int, columns_to_show: list[schema.List_cols] = Query(...), db: Session = Depends(crud.get_db)):
+    return crud.get_by_page(page, columns_to_show, db)
+
+@app.get("/data/filterMMA", tags=["get"])
+def max_of(column_to_filter: schema.List_cols = Query(...), filter_value: schema.listMMA = Query(...),  db: Session = Depends(crud.get_db)):
+    return crud.get_MMA_of(column_to_filter, filter_value, db)
+
+@app.get("/data/multi_filter", tags=["get"])
+def filter_with_value(columns_to_show: list[schema.List_cols] = Query(...), columns_to_filter: list[schema.List_cols] = Query(...),
+filters_values: list[float] = Query(...), filters_symbols: tuple[schema.list_symbols] = Query(...), db: Session = Depends(crud.get_db)):  
+    return crud.return_query(columns_to_show, columns_to_filter, filters_symbols, filters_values, db)
+
+@app.get("data/range_filter", tags=["get"])
+def filter_in_range(columns_to_show: list[schema.List_cols] = Query(...), columns_to_filter_by_range: list[schema.List_cols] = Query(...),
+symbol1: tuple[schema.list_symbols] = Query(...), filters_values1: list[float] = Query(...), symbol2: tuple[schema.list_symbols] = Query(...),
+filters_values2: list[float] = Query(...), num_results:int = Query(...), db: Session = Depends(crud.get_db)):
+    return crud.return_query(columns_to_show, columns_to_filter_by_range, filters_values1, symbol1, db, num_results, columns_to_filter_by_range, filters_values2, symbol2)
 
 
 if __name__ == '__main__':
--- api/schema.py
+++ api/schema.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
+from enum import Enum
+
 
 class DataBase(BaseModel):
     baseline_value: int
@@ -31,3 +33,40 @@ class Data(DataBase):
     id: int
     class Config:
         orm_mode = True
+
+class List_cols(str, Enum):
+    baseline_value = "baseline_value"
+    accelerations = "accelerations"
+    fetal_movement = "fetal_movement"
+    uterine_contractions = "uterine_contractions"
+    light_decelerations = "light_decelerations"
+    severe_decelerations = "severe_decelerations"
+    prolongued_decelerations = "prolongued_decelerations"
+    abnormal_short_term_variability = "abnormal_short_term_variability"
+    mean_value_of_short_term_variability = "mean_value_of_short_term_variability"
+    percentage_of_time_with_abnormal_long_term_variability = "percentage_of_time_with_abnormal_long_term_variability"
+    mean_value_of_long_term_variability = "mean_value_of_long_term_variability"
+    histogram_width = "histogram_width"
+    histogram_min = "histogram_min"
+    histogram_max = "histogram_max"
+    histogram_number_of_peaks = "histogram_number_of_peaks"
+    histogram_number_of_zeroes = "histogram_number_of_zeroes"
+    histogram_mode = "histogram_mode"
+    histogram_mean = "histogram_mean"
+    histogram_median = "histogram_median"
+    histogram_variance = "histogram_variance"
+    histogram_tendency = "histogram_tendency"
+    fetal_health = "fetal_health"
+
+class listMMA(str, Enum):
+    max = "max"
+    min = "min"
+    avg = "avg"
+
+class list_symbols(str, Enum):
+    smaller = "<"
+    larger = ">"
+    equal = "=="
+    smaller_equal = "=<"
+    larger_equal = "=>"
+    not_equal = "!="
